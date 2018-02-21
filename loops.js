bd4993df68caf61dffb7aaca4dcbfd3abe06ac97
Michael Losada <michael.losada@gmail.com>
2018-02-21 15:24:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,9 +2,9 @@
  function forLoop(array){
    for (let i = 0; i < 25; i++){
       if (i===1){
-      console.log("I am 1 strange loop")
-    }else{
-      console.log(`I am ${i} strange loops`)
+      console.log(`I am ${i} strange loop`);
+    } else{
+      console.log(`I am ${i} strange loops`);
     }
   }
  }  
